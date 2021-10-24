18f938182e97045d81b932d41beb7877a1cf1752
Kyle Madsen <kyle.mdsn@gmail.com>
2021-10-24 11:02:22 -0700

    Add more basic generics

--- hello_world/hello_generics/src/point.rs
+++ hello_world/hello_generics/src/point.rs
@@ -20,15 +20,45 @@ impl Point<f32> {
   }
 }
 
-impl Point<u32> {
+impl Point<i32> {
   pub fn magnitude(&self) -> f32 {
       ((self.x.pow(2) + self.y.pow(2)) as f32).sqrt()
+  }
+}
+
+impl std::cmp::PartialEq for Point<f32> {
+  fn eq(&self, other: &Self) -> bool {
+    self.x.eq(&other.x) && self.y.eq(&other.y)
+  }
+}
+
+impl std::cmp::PartialOrd for Point<f32> {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    self.magnitude().partial_cmp(&other.magnitude())
+  }
+}
+
+impl std::cmp::PartialEq for Point<i32> {
+  fn eq(&self, other: &Self) -> bool {
+    self.x.eq(&other.x) && self.y.eq(&other.y)
+  }
+}
+
+impl std::cmp::PartialOrd for Point<i32> {
+  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    self.magnitude().partial_cmp(&other.magnitude())
   }
 }
 
 impl<T : std::fmt::Display> Point<T> {
   pub fn json(&self) -> std::string::String {
       format!("{{x:{},y:{}}}", self.x, self.y)
+  }
+}
+
+impl <T : std::fmt::Display> std::fmt::Display for Point<T> {
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "{}", self.json())
   }
 }
 
@@ -79,5 +109,29 @@ mod test {
     let float = Point { x: 5.1, y: 10.89 };
 
     assert_eq_delta!(12.025, float.magnitude(), 0.0001);
+  }
+
+  #[test]
+  fn veify_float_equality() {
+    let float_lhs = Point { x: 5.1, y: 10.89 };
+    let float_rhs = Point { x: 5.1, y: 10.89 };
+
+    assert_eq!(true, float_lhs == float_rhs)
+  }
+
+  #[test]
+  fn veify_integer_inequality() {
+    let float_lhs = Point { x: 1, y: 2 };
+    let float_rhs = Point { x: -1, y: -2 };
+
+    assert_eq!(true, float_lhs != float_rhs)
+  }
+
+  #[test]
+  fn veify_float_magnitude_compare() {
+    let float_lhs = Point { x: 1, y: 2 };
+    let float_rhs = Point { x: -1, y: -4 };
+
+    assert_eq!(true, float_lhs < float_rhs)
   }
 }
