d9b75793a4db7855da31743edd750727d4026453
Kyle Madsen <kyle.mdsn@gmail.com>
2021-11-02 08:21:25 -0700

    Add more hello-cli examples

--- hello_world/hello_cargo/README.md
+++ hello_world/hello_cargo/README.md
@@ -3,28 +3,52 @@
 Cargo is a Rust build system and package manager.
 
 ## Create
-
+```
 $ cargo new hello_cargo
 $ cd hello_cargo
 $ cargo check
-
+```
 ## Build
 
+```
 $ cargo build
-
+```
 ## Run
 
+```
 $ ./target/debug/hello_cargo
 $ cargo run
-
+```
 ## Integration tests
 
 This integrates with the hello_crate library.
 
+```
 $ cargo test
+```
 
 ## Useful tools
 
+```
 $ cargo fmt
 $ cargo fix
 $ cargo clippy
+```
+
+## Adding dependencies
+
+Cargo does not have an add command. But if you have cargo-edit there is.
+You can also manage your Cargo.toml directly.
+
+```
+$ cargo add plotters
+PS C:\Users\Travis\Development\rust\adb-sim\cli> cargo add plotters
+error: no such subcommand: `add`
+
+        Did you mean `d`?
+```
+
+```
+$ cargo install cargo-edit
+$ cargo add plotters
+```
--- hello_world/hello_cli/grrs/src/main.rs
+++ hello_world/hello_cli/grrs/src/main.rs
@@ -1,4 +1,5 @@
 use structopt::StructOpt;
+use std::str::FromStr;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -6,15 +7,45 @@ struct Cli {
 
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+
+    #[structopt(default_value = "normal")]
+    method: Method,
+}
+
+#[derive(StructOpt)]
+enum Method {
+    Normal,
+    Anyhow
+}
+
+// any error type implementing Display is acceptable.
+type ParseError = &'static str;
+
+impl FromStr for Method {
+    type Err = ParseError;
+    fn from_str(day: &str) -> Result<Self, Self::Err> {
+        match day {
+            "normal" => Ok(Method::Normal),
+            "anyhow" => Ok(Method::Anyhow),
+            _ => Err("Could not parse a day"),
+        }
+    }
 }
 
 mod lib;
 
 fn main() {
     let args = Cli::from_args();
-    lib::find_matches(
-        &args.path,
-        &args.pattern,
-        &mut std::io::stdout()
-    ).unwrap();
+    match &args.method {
+        Method::Normal => lib::find_matches(
+            &args.path,
+            &args.pattern,
+            &mut std::io::stdout()
+        ).unwrap(),
+        Method::Anyhow => lib::find_matches_anyhow(
+            &args.path,
+            &args.pattern,
+            &mut std::io::stdout()
+        ).unwrap()
+    }
 }
