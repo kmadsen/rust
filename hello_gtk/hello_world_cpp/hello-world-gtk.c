f3e0f4811573458ea87a71508bc5ede1d959b28e
Kyle Madsen <kyle.mdsn@gmail.com>
2022-01-20 07:59:59 -0800

    Testing gtk in cpp and rust

---
+++ hello_gtk/hello_world_cpp/hello-world-gtk.c
@@ -0,0 +1,239 @@
+// VsCode having an issue includeing this
+#include <gtk/gtk.h>
+
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+static GtkWidget*
+build_grid (GtkWidget *window)
+{
+  GtkWidget *grid;
+  GtkWidget *button;
+
+  /* Here we construct the container that is going pack our buttons */
+  grid = gtk_grid_new();
+  button = gtk_button_new_with_label ("Button 1");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  /* Place the first button in the grid cell (0, 0), and make it fill
+   * just 1 cell horizontally and vertically (ie no spanning)
+   */
+  gtk_grid_attach (GTK_GRID (grid), button, 0, 0, 1, 1);
+
+  button = gtk_button_new_with_label ("Button 2");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+
+  /* Place the second button in the grid cell (1, 0), and make it fill
+   * just 1 cell horizontally and vertically (ie no spanning)
+   */
+  gtk_grid_attach (GTK_GRID (grid), button, 1, 0, 1, 1);
+
+  button = gtk_button_new_with_label ("Quit");
+  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_window_destroy), window);
+
+  /* Place the Quit button in the grid cell (0, 1), and make it
+   * span 2 columns.
+   */
+  gtk_grid_attach (GTK_GRID (grid), button, 0, 1, 2, 1);
+
+  return grid;
+}
+
+/* Surface to store current scribbles */
+static cairo_surface_t *surface = NULL;
+
+static void
+clear_surface (void)
+{
+  cairo_t *cr;
+
+  cr = cairo_create (surface);
+
+  cairo_set_source_rgb (cr, 1, 1, 1);
+  cairo_paint (cr);
+
+  cairo_destroy (cr);
+}
+
+/* Create a new surface of the appropriate size to store our scribbles */
+static void
+resize_cb (GtkWidget *widget,
+           int        width,
+           int        height,
+           gpointer   data)
+{
+  if (surface)
+    {
+      cairo_surface_destroy (surface);
+      surface = NULL;
+    }
+
+  if (gtk_native_get_surface (gtk_widget_get_native (widget)))
+    {
+      surface = gdk_surface_create_similar_surface (gtk_native_get_surface (gtk_widget_get_native (widget)),
+                                                    CAIRO_CONTENT_COLOR,
+                                                    gtk_widget_get_width (widget),
+                                                    gtk_widget_get_height (widget));
+
+      /* Initialize the surface to white */
+      clear_surface ();
+    }
+}
+
+/* Redraw the screen from the surface. Note that the draw
+ * callback receives a ready-to-be-used cairo_t that is already
+ * clipped to only draw the exposed areas of the widget
+ */
+static void
+draw_cb (GtkDrawingArea *drawing_area,
+         cairo_t        *cr,
+         int             width,
+         int             height,
+         gpointer        data)
+{
+  cairo_set_source_surface (cr, surface, 0, 0);
+  cairo_paint (cr);
+}
+
+/* Draw a rectangle on the surface at the given position */
+static void
+draw_brush (GtkWidget *widget,
+            double     x,
+            double     y)
+{
+  cairo_t *cr;
+
+  /* Paint to the surface, where we store our state */
+  cr = cairo_create (surface);
+
+  cairo_rectangle (cr, x - 3, y - 3, 6, 6);
+  cairo_fill (cr);
+
+  cairo_destroy (cr);
+
+  /* Now invalidate the drawing area. */
+  gtk_widget_queue_draw (widget);
+}
+
+static double start_x;
+static double start_y;
+
+static void
+drag_begin (GtkGestureDrag *gesture,
+            double          x,
+            double          y,
+            GtkWidget      *area)
+{
+  start_x = x;
+  start_y = y;
+
+  draw_brush (area, x, y);
+}
+
+static void
+drag_update (GtkGestureDrag *gesture,
+             double          x,
+             double          y,
+             GtkWidget      *area)
+{
+  draw_brush (area, start_x + x, start_y + y);
+}
+
+static void
+drag_end (GtkGestureDrag *gesture,
+          double          x,
+          double          y,
+          GtkWidget      *area)
+{
+  draw_brush (area, start_x + x, start_y + y);
+}
+
+static void
+pressed (GtkGestureClick *gesture,
+         int              n_press,
+         double           x,
+         double           y,
+         GtkWidget       *area)
+{
+  clear_surface ();
+  gtk_widget_queue_draw (area);
+}
+
+static GtkWidget*
+build_frame (GtkWidget *window)
+{
+  GtkWidget *frame;
+  GtkWidget *drawing_area;
+  GtkGesture *drag;
+  GtkGesture *press;
+  
+  frame = gtk_frame_new (NULL);
+
+  drawing_area = gtk_drawing_area_new ();
+  /* set a minimum size */
+  gtk_widget_set_size_request (drawing_area, 100, 100);
+
+  gtk_frame_set_child (GTK_FRAME (frame), drawing_area);
+
+  gtk_drawing_area_set_draw_func (GTK_DRAWING_AREA (drawing_area), draw_cb, NULL, NULL);
+
+  g_signal_connect_after (drawing_area, "resize", G_CALLBACK (resize_cb), NULL);
+
+  drag = gtk_gesture_drag_new ();
+  gtk_gesture_single_set_button (GTK_GESTURE_SINGLE (drag), GDK_BUTTON_PRIMARY);
+  gtk_widget_add_controller (drawing_area, GTK_EVENT_CONTROLLER (drag));
+  g_signal_connect (drag, "drag-begin", G_CALLBACK (drag_begin), drawing_area);
+  g_signal_connect (drag, "drag-update", G_CALLBACK (drag_update), drawing_area);
+  g_signal_connect (drag, "drag-end", G_CALLBACK (drag_end), drawing_area);
+
+  press = gtk_gesture_click_new ();
+  gtk_gesture_single_set_button (GTK_GESTURE_SINGLE (press), GDK_BUTTON_SECONDARY);
+  gtk_widget_add_controller (drawing_area, GTK_EVENT_CONTROLLER (press));
+
+  g_signal_connect (press, "pressed", G_CALLBACK (pressed), drawing_area);
+
+  return frame;
+}
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+
+  /* create a new window, and set its title */
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+
+  GtkWidget *grid = build_grid(window);
+  GtkWidget *frame = build_frame(window);
+
+  /* Place the drawing frame in the grid cell (0, 2), and make it
+   * span 2 columns and 2 rows.
+   */
+  gtk_grid_attach (GTK_GRID (grid), frame, 0, 2, 2, 2);
+
+  /* Pack the container in the window */
+  gtk_window_set_child (GTK_WINDOW (window), grid);
+
+  gtk_widget_show (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
